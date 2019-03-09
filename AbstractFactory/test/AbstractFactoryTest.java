b03ebe747640f8a2f3f9fb31e81408037fd5d571
qishengking <68449518@qq.com>
2019-03-09 19:42:45 +0800

    测试下上传作业

---
+++ AbstractFactory/test/AbstractFactoryTest.java
@@ -0,0 +1,12 @@
+package com.gupao.AbstractFactory.test;
+
+import com.gupao.AbstractFactory.XiaoMiFactory;
+import com.gupao.Factory.XiaoMi;
+
+public class AbstractFactoryTest {
+    public static void main(String[] args){
+        XiaoMiFactory factory = new XiaoMiFactory();
+        factory.createCall().call();
+        factory.createMessige().messige();
+    }
+}
